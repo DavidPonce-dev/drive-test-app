25314943ef15f28b07105ee4d7208b804724789a
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2021-01-08 02:30:11 -0300

    generando cuestionario

--- src/views/Cuestionario.vue
+++ src/views/Cuestionario.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>Cuestionario</h1>
+  <div class="container-fluid row bg-warning">
+    <div class="col-sm-5 col-md-4 col-lg-3">
+      <LeftBar></LeftBar>
+    </div>
+    <div class="col-sm"></div>
+    <div class="col-sm-4 col-md-3">
+      <RightBar />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import RightBar from "../components/RightBar.vue";
+import LeftBar from "../components/LeftBar.vue";
+import { mapGetters } from "vuex";
 
-<style></style>
+export default {
+  components: {
+    LeftBar,
+    RightBar
+  },
+  computed: {
+    ...mapGetters({
+      user: "user"
+    })
+  }
+};
+</script>
+<style scoped>
+.titulo {
+  text-align: center;
+  margin: 20px 0;
+}
+.row > * {
+  margin: 0;
+  padding: 0;
+}
+.card {
+  margin: 15px;
+}
+</style>
